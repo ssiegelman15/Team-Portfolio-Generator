394ebe6697798f382f2260e3bd4de5d82b231ab6
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-22 15:44:21 -0600

    created html template file and linked it to index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateTemplate = require('./src/generateTemplate.js');
 
-const prompts = [
+const questions = [
   {
 
   }
 ];
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((input) => {
+      writeToFile('GenerateTeamPortfolio.html', generateTemplate(input))
+    })
+}
+
+// Function call to initialize app
+init();
