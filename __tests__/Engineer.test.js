3f07faed65872a8124a6e7354ba4f62075204fe5
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-23 21:19:16 -0600

    created intern test file, basic testing is complete for classes

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,20 +1,21 @@
 const Engineer = require('../lib/Engineer');
 
 describe('Initialization', () => {
-  it("Should create object with name, id and email if provided", () => {
+  it("Should create object with name, id, email & github username if provided", () => {
     const engineer = new Engineer('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com', 'ssiegelman15');
 
     // Verify that new object has correct properties
     expect(engineer.name).toEqual("Shlomo");
     expect(engineer.id).toEqual("ssiegelman");
     expect(engineer.email).toEqual("ssiegelman15@gmail.com");
+    expect(engineer.github).toEqual('ssiegelman15');
   })
 
-  it("Should create object with office number if provided", () => {
+  it("Should return Github username", () => {
     const engineer = new Engineer('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com', 'ssiegelman15');
 
     // Verify that new object has correct properties
-    expect(engineer.github).toEqual('ssiegelman15');
+    expect(engineer.getGithub()).toEqual('ssiegelman15');
   })
 
   it("getRole function should now return 'Engineer' instead of 'Employee'", () => {
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,27 @@
+const Intern = require('../lib/intern');
+
+describe('Initialization', () => {
+  it("Should create object with name, id, email & school if provided", () => {
+    const intern = new Intern('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com', 'Rutgers');
+
+    // Verify that new object has correct properties
+    expect(intern.name).toEqual("Shlomo");
+    expect(intern.id).toEqual("ssiegelman");
+    expect(intern.email).toEqual("ssiegelman15@gmail.com");
+    expect(intern.school).toEqual("Rutgers");
+  })
+
+  it("Should return name of school", () => {
+    const intern = new Intern('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com', 'Rutgers');
+
+    // Verify that new object has correct properties
+    expect(intern.getSchool()).toEqual('Rutgers');
+  })
+
+  it("getRole function should now return 'intern' instead of 'Employee'", () => {
+    const intern = new Intern('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com', 'Rutgers');
+
+    // Verify that getRole function returns 'intern'
+    expect(intern.getRole()).toEqual('Intern')
+  })
+})
