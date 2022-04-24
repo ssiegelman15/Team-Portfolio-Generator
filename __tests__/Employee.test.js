a448ebccb2cf83ddcd52429c2689756bf61723cc
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-23 20:22:18 -0600

    added jest to package.json and first test for Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,12 @@
 const Employee = require('../lib/Employee');
+
+describe('Initialization', () => {
+  it("Should create object with name, id and email if provided", () => {
+    const employee = new Employee('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com');
+
+    // Verify that new object has correct properties
+    expect(employee.name).toEqual("Shlomo");
+    expect(employee.id).toEqual("ssiegelman");
+    expect(employee.email).toEqual("ssiegelman15@gmail.com");
+  })
+});
