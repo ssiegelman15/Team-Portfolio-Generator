ddb5d38ba4e2cb65d0b26be5576c0bcae1bbb812
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-23 20:59:12 -0600

    wrote initialization tests for Manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,19 @@
 const Manager = require('../lib/Manager');
 
+describe('Initialization', () => {
+  it("Should create object with name, id and email if provided", () => {
+    const manager = new Manager('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com', 15);
+
+    // Verify that new object has correct properties
+    expect(manager.name).toEqual("Shlomo");
+    expect(manager.id).toEqual("ssiegelman");
+    expect(manager.email).toEqual("ssiegelman15@gmail.com");
+  })
+
+  it("Should create object with office number if provided", () => {
+    const manager = new Manager('Shlomo', 'ssiegelman', 'ssiegelman15@gmail.com', 15);
+
+    // Verify that new object has correct properties
+    expect(manager.officeNumber).toEqual(15);
+  })
+})
