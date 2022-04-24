bade1946758bb98986ccb5b7e35a212a4085b70b
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-24 12:18:44 -0600

    updated font-awesome call to run faster, updated html generator code

--- generateHTML.js
+++ generateHTML.js
@@ -20,15 +20,30 @@ function renderManager(manager) {
 function generateTemplate(data) {
 return `<!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <title>Team Portfolio</title>
 </head>
+
 <body>
-  
+  <header class="bg-info text-white text-center">
+    <h1 class="p-5">My Team</h1>
+  </header>
+
+  <main>
+    <div class='justify-content-center card-deck'>
+      <!-- Where employee cards will populate -->
+    </div>
+  </main>
+  <script src="https://kit.fontawesome.com/0e1a0a5b63.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
 `;
 }
