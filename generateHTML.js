b6d490446125a50448fa1cebc0a526c91c193bd3
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-24 11:18:23 -0600

    working on creating mockup HTML and rendering functions for each employee

--- generateHTML.js
+++ generateHTML.js
@@ -1,3 +1,21 @@
+// Create Manager card to be appended to the HTML
+function renderManager(manager) {
+  return `
+<div class="card col-3">
+  <div class="card-title">
+      <h2>${manager.getName()}</h2>
+      <h3><i class="fas fa-crown"></i> ${manager.getRole()}</h3>
+  </div>
+  <div class="card-body">
+      <ul class="list-group">
+          <li class="list-group-item">ID: ${manager.getId()}</li>
+          <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+          <li class="list-group-item">Office number: <a href="tel:${manager.officeNumber}">${manager.officeNumber}</a></li>
+      </ul>
+  </div>
+</div>`;
+}
+
 // Function to build HTML file using user input
 function generateTemplate(data) {
 return `<!DOCTYPE html>
