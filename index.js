86c5fc8e92ddc8ec8ba22d60ccfcf8d68279b26c
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-23 21:59:20 -0600

    called Engineer, Intern & Manager class files into index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateTemplate = require('./generateTemplate.js');
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
 
 // List of questions to ask user
 const questions = [
